af2bf28105b6e5a7b4423518b2ce2494dcacff87
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-05-02 00:00:40 +0200

    feat: add hero

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -2,46 +2,28 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 export const ParallaxSection = () => {
   return (
-    <Parallax pages={2} style={{ top: "500", left: "0" }}>
-      <ParallaxLayer offset={0.3} speed={-0.7}>
-        <div
-          id="supernovas"
-          className="w-full h-[700px] bg-[url('/images/Supernovas.png')] bg-no-repeat bg-contain bg-center"
-        ></div>
-      </ParallaxLayer>
+    <>
+      <Parallax pages={2} style={{ top: "0", left: "0" }} className="relative">
+        <ParallaxLayer offset={0} speed={-0.7}>
+          {/* <div
+            id="bottom"
+            className="w-full bg-[url('/images/bottom.png')] bg-no-repeat bg-cover bg-center"
+          ></div> */}
+          <img id="bottom" src="/images/bottom.png" className="w-full"></img>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={-0.15}>
-        <div
-          id="venus"
-          className="w-full h-[700px] bg-[url('/images/Venus-Disgustada.png')] bg-contain bg-no-repeat bg-center"
-        ></div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0.1} speed={-0.1}>
-        <div
-          id="carmen"
-          className="w-full h-[700px] bg-[url('/images/Carmen-Risa.png')] bg-no-repeat bg-contain bg-center"
-        ></div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0.1} speed={-0.2}>
-        <div
-          id="twinky"
-          className="w-full h-[700px] bg-[url('/images/Twinky-Risa.png')] bg-no-repeat bg-contain bg-center"
-        ></div>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={-0.3}>
-        <div
-          id="guille"
-          className="w-full h-[700px] bg-[url('/images/Guille-Relata.png')] bg-no-repeat bg-contain bg-center"
-        ></div>
-      </ParallaxLayer>
-
-      {/* capa logo */}
-      <ParallaxLayer offset={0.5} speed={1}>
-        <div
-          id="logo"
-          className="w-full h-[700px] bg-[url('/images/Logo-Rave.png')] bg-no-repeat bg-contain bg-center"
-        ></div>
-      </ParallaxLayer>
-    </Parallax>
+        <ParallaxLayer offset={0.5} speed={-0.15}>
+          {/* <div
+            id="assets"
+            className="w-full h-[700px] bg-[url('/images/assets.png')] bg-contain bg-no-repeat bg-center"
+          ></div> */}
+          <img
+            id="bottom"
+            src="/images/assets.png"
+            className="absolute w-full"
+          ></img>
+        </ParallaxLayer>
+      </Parallax>
+    </>
   );
 };
