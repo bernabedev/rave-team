ec7533ddf71e429668d68775b1a63d7141adb289
Erick Vargas <erianvc.dev@gmail.com>
2025-05-02 13:01:36 -0500

    feat: generate icon names type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import icon from 'astro-icon'
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
-import react from '@astrojs/react';
+import { iconTyping } from './scripts/integrations.ts'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), iconTyping()],
   vite: {
     plugins: [tailwindcss()],
   },
