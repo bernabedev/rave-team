c24925683b6e2f87bf034baa945a0941a96e809b
developerleonardo <leo.11hnz@gmail.com>
2025-05-03 21:10:19 -0500

    feat: remove alignment property from NewsCard and update news data structure

--- src/data/news.ts
+++ src/data/news.ts
@@ -6,20 +6,17 @@ export const news: Array<newsType> = [
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
-    align: "end",
   },
   {
     title: "NEW 2",
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
-    align: "self-end",
   },
   {
     title: "NEW 3",
     description:
       "A game development studio creating projects that primarily we would play ourselves first, and then aim at massive audiences. A game development studio creating projects that primarily we would",
     link: "/",
-    align: "end",
   },
 ];
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,4 @@ export interface newsType {
   title: string;
   description: string;
   link: string;
-  align: "end" | "self-end";
 }
