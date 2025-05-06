94b109c84ba170ca4e160be82ed420d59d47ea91
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-05-06 23:56:35 +0200

    feat: intento de formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import { iconTyping } from './scripts/integrations.ts'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react(), iconTyping()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
